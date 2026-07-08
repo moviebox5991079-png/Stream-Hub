85448558b752a48a54d16670b54063ff0be31e34
moviebox5991079-png <moviebox5991079@gmail.com>
2026-07-09 00:17:39 +0500

    Update HomeClient.tsx

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -342,7 +342,7 @@ export default function HomeClient({ initialData }: HomeProps) {
                          </div>
 
                          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 relative z-10">
-                           {availableStreams.map((stream, idx) => (
+                           {.map((stream, idx) => (
                              <div
                                key={idx}
                                onClick={() => {
@@ -489,7 +489,7 @@ export default function HomeClient({ initialData }: HomeProps) {
                )}
 
                {/* BOTTOM STREAMS GRID */}
-               {selectedVideo && availableStreams && availableStreams.length > 0 && (
+               {selectedVideo &&  && .length > 0 && (
                  <>
                    <div className="flex flex-col mt-8 mb-4 px-1">
                      <div className="flex justify-center my-6 w-full overflow-hidden">
@@ -522,17 +522,7 @@ export default function HomeClient({ initialData }: HomeProps) {
                                  setIsChangingChannel(false);
                               }, 1000);
                           }}
-                           {/* onClick={() => { 
-                             setIsChangingChannel(true);
-                             setSelectedVideo(video); 
-                             // ✨ Yahan bhi forceAutoPlay TRUE kar diya taake bottom se match badalne par direct chale
-                             setForceAutoPlay(true);
-                             window.scrollTo({ top: 0, behavior: 'smooth' }); 
-                             
-                             setTimeout(() => {
-                               setIsChangingChannel(false);
-                             }, 1000);
-                           }}  */}
+ 
                            className="group cursor-pointer rounded-2xl overflow-hidden bg-transparent transition-all duration-500 hover:-translate-y-2 relative shadow-[0_10px_30px_rgba(0,0,0,0.8)] flex flex-col"
                          >
                             <div className="absolute inset-0 rounded-2xl animate-rainbow opacity-50 group-hover:opacity-100 transition-opacity duration-500 -z-10 p-[2px]">
